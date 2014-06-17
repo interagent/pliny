85063b279d28279b595e184908f5481c56110c1f
Tobias L. Maier <tobias.maier@baucloud.com>
2014-06-17 22:23:22 +0200

    Add paginator helper

---
+++ lib/pliny/helpers/paginator.rb
@@ -0,0 +1,34 @@
+module Pliny::Helpers
+  module Paginator
+    def paginator(count, options = {})
+      options =
+        {
+          accepted_ranges: :id,
+          order: :id,
+          start: 0,
+          max: 200
+        }.merge(options)
+
+      options[:end] = options[:start] + options[:max] - 1
+
+      halt(400) unless options[:accepted_ranges].include?(options[:order].to_sym)
+
+      headers 'Accept-Ranges' => options[:accepted_ranges].join(',')
+
+      if count > options[:max]
+        status 206
+        headers \
+          'Content-Range' => "#{options[:order]} #{options[:start]}..#{options[:end]}/#{count}; max=#{options[:max]}",
+          'Next-Range' => "#{options[:order]} #{options[:end] + 1}..#{options[:end] + options[:max]}; max=#{options[:max]}"
+      else
+        status 200
+      end
+
+      {
+        order: options[:order],
+        start: options[:start],
+        limit: options[:max]
+      }
+    end
+  end
+end
