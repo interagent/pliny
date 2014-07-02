91c5425b48fdf865b12434faf02c002c36ea5905
Tobias L. Maier <me@tobiasmaier.info>
2014-07-02 12:30:14 +0200

    Add test for #uuid_paginator

--- test/helpers/paginator_test.rb
+++ test/helpers/paginator_test.rb
@@ -11,6 +11,15 @@ describe Pliny::Helpers::Paginator do
     end
   end
 
+  describe '#uuid_paginator' do
+    it 'calls #paginator' do
+      resource = Class.new
+      stub(resource).count { 12 }
+      mock(sinatra).paginator(12, sort_by: :foo)
+      sinatra.uuid_paginator(resource, sort_by: :foo)
+    end
+  end
+
   describe '#integer_paginator' do
     it 'calls IntegerPaginator.run' do
       mock(Pliny::Helpers::Paginator::IntegerPaginator).run(sinatra, 12, sort_by: :foo)
