cafe7219ac4fcefe6db431328bc81a11b444003a
Will Leinweber <will@bitfission.com>
2014-08-15 13:29:27 -0700

    Casting ConfigHelpers

--- lib/pliny/config_helpers.rb
+++ lib/pliny/config_helpers.rb
@@ -1,30 +1,43 @@
 module Pliny
   module ConfigHelpers
-    def optional(*attrs)
-      attrs.each do |attr|
-        instance_eval "def #{attr}; @#{attr} ||= ENV['#{attr.upcase}'] end", __FILE__, __LINE__
-        ConfigHelpers.add_question_method(attr)
-      end
+    def mandatory(name, method=nil)
+      value = cast(ENV.fetch(name.to_s.upcase), method)
+      create(name, value)
     end
 
-    def mandatory(*attrs)
-      attrs.each do |attr|
-        instance_eval "def #{attr}; @#{attr} ||= ENV['#{attr.upcase}'] || raise('missing=#{attr.upcase}') end", __FILE__, __LINE__
-        ConfigHelpers.add_question_method(attr)
-      end
+    def optional(name, method=nil)
+      value = cast(ENV[name.to_s.upcase], method)
+      create(name, value)
     end
 
-    def override(attrs)
-      attrs.each do |attr, value|
-        instance_eval "def #{attr}; @#{attr} ||= ENV['#{attr.upcase}'] || '#{value}'.to_s end", __FILE__, __LINE__
-        ConfigHelpers.add_question_method(attr)
-      end
+    def override(name, default, method=nil)
+      value = cast(ENV.fetch(name.to_s.upcase, default), method)
+      create(name, value)
     end
 
-    def self.add_question_method(attr)
-      define_method "#{attr}?" do
-        return false if send(attr).nil?
-        !!(send(attr) =~ /\Atrue|yes|on\z/i || send(attr).to_i > 0)
+    def int
+      ->(v) { v.to_i }
+    end
+
+    def float
+      ->(v) { v.to_f }
+    end
+
+    def bool
+      ->(v) { v.to_s=='true'}
+    end
+
+    private
+
+    def cast(value, method)
+      method ? method.call(value) : value
+    end
+
+    def create(name, value)
+      instance_variable_set(:"@#{name}", value)
+      instance_eval "def #{name}; @#{name} end", __FILE__, __LINE__
+      if value.kind_of?(TrueClass) || value.kind_of?(FalseClass) || value.kind_of?(NilClass)
+        instance_eval "def #{name}?; !!@#{name} end", __FILE__, __LINE__
       end
     end
   end
