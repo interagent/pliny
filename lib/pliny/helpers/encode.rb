5db11ca2961f68c43d89b9191a2fae35024e807e
Tobias L. Maier <me@tobiasmaier.info>
2014-09-04 16:30:59 +0200

    Fix code style

--- lib/pliny/helpers/encode.rb
+++ lib/pliny/helpers/encode.rb
@@ -1,8 +1,7 @@
 module Pliny::Helpers
   module Encode
     def encode(object)
-      MultiJson.encode object,
-                       pretty: params[:pretty] == 'true' || Config.pretty_json
+      MultiJson.encode(object, pretty: params[:pretty] == 'true' || Config.pretty_json)
     end
   end
 end
