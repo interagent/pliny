6a83b2edda067de5d31ac2d0ee66d5e45b5b4e7d
Tobias L. Maier <me@tobiasmaier.info>
2014-09-05 00:38:15 +0200

    Upcase description

--- lib/pliny/commands/generator.rb
+++ lib/pliny/commands/generator.rb
@@ -2,7 +2,7 @@ require 'thor'
 
 module Pliny::Commands
   class Generator < Thor
-    desc 'endpoint NAME', 'generates an endpoint'
+    desc 'endpoint NAME', 'Generates an endpoint'
     method_option :scaffold, type: :boolean, default: false, hide: true
     def endpoint(name)
       ep = Endpoint.new(name, options)
@@ -11,20 +11,20 @@ module Pliny::Commands
       ep.create_endpoint_acceptance_test
     end
 
-    desc 'mediator NAME', 'generates a mediator'
+    desc 'mediator NAME', 'Generates a mediator'
     def mediator(name)
       md = Mediator.new(name, options)
       md.create_mediator
       md.create_mediator_test
     end
 
-    desc 'migration NAME', 'generates a migration'
+    desc 'migration NAME', 'Generates a migration'
     def migration(name)
       mg = Migration.new(name, options)
       mg.create_migration
     end
 
-    desc 'model NAME', 'generates a model'
+    desc 'model NAME', 'Generates a model'
     method_option :paranoid, type: :boolean, default: false, desc: 'adds paranoid support to model'
     def model(name)
       md = Model.new(name, options)
@@ -33,7 +33,7 @@ module Pliny::Commands
       md.create_model_test
     end
 
-    desc 'scaffold NAME', 'generates a scaffold of endpoint, model, schema and serializer'
+    desc 'scaffold NAME', 'Generates a scaffold of endpoint, model, schema and serializer'
     method_option :paranoid, type: :boolean, default: false, desc: 'adds paranoid support to model'
     method_option :scaffold, type: :boolean, default: true, hide: true
     def scaffold(name)
@@ -43,14 +43,14 @@ module Pliny::Commands
       serializer(name)
     end
 
-    desc 'schema NAME', 'generates a schema'
+    desc 'schema NAME', 'Generates a schema'
     def schema(name)
       sc = Schema.new(name, options)
       sc.create_schema
       sc.rebuild_schema
     end
 
-    desc 'serializer NAME', 'generates a serializer'
+    desc 'serializer NAME', 'Generates a serializer'
     def serializer(name)
       se = Serializer.new(name, options)
       se.create_serializer
