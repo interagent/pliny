980db63e57354131a269f34d5bfb07eead53e008
Tobias L. Maier <me@tobiasmaier.info>
2014-09-04 15:03:38 +0200

    Add Encode helper

---
+++ lib/pliny/helpers/encode.rb
@@ -0,0 +1,8 @@
+module Pliny::Helpers
+  module Encode
+    def encode(object)
+      MultiJson.encode object,
+                       pretty: params[:pretty] == 'true' || Config.pretty_json
+    end
+  end
+end
