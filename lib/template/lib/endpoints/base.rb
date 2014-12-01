a69f552388cca4a842c0ec25c87d508cdb7034e4
Tobias L. Maier <me@tobiasmaier.info>
2014-12-01 16:11:27 +0100

    Add paginator to Endpoints::Base
    Original pull request: https://github.com/interagent/pliny-template/pull/120

--- lib/template/lib/endpoints/base.rb
+++ lib/template/lib/endpoints/base.rb
@@ -6,6 +6,7 @@ module Endpoints
 
     helpers Pliny::Helpers::Encode
     helpers Pliny::Helpers::Params
+    helpers Pliny::Helpers::Paginator
 
     set :dump_errors, false
     set :raise_errors, true
