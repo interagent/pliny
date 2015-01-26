cd627efc08fce0fc988f595033257db4256b3add
Dan Peterson <dpiddy@gmail.com>
2015-01-26 10:59:01 -0400

    Only check spec.described_class if set.
    This allows a string as the argument to `describe`.

--- lib/template/spec/support/auto_define_rack_app.rb
+++ lib/template/spec/support/auto_define_rack_app.rb
@@ -1,7 +1,7 @@
 RSpec.configure do |config|
   config.before(:context) do |spec|
     # weird ruby syntax, but test if the described_class inherits Sinatra::Base:
-    if !@rack_app && spec.described_class < Sinatra::Base
+    if !@rack_app && spec.described_class && spec.described_class < Sinatra::Base
       @rack_app = spec.described_class
     end
   end
