4953d9bc2274c4dd665d96c3efad4a955f8b2ce4
Neil Middleton <neil@heroku.com>
2015-03-24 16:17:56 +0000

    Use seperate database_timeout versus timeout

--- lib/template/config/initializers/database.rb
+++ lib/template/config/initializers/database.rb
@@ -1,3 +1,3 @@
 Sequel.connect(Config.database_url,
                max_connections: Config.db_pool,
-               after_connect: -> (c) { c.execute "set statement_timeout = '#{Config.timeout}s'"  })
+               after_connect: -> (c) { c.execute "set statement_timeout = '#{Config.database_timeout}s'"  })
