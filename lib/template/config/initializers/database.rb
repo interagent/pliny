8bd25d5c89fb3bb0aa7482f44389d2f4506eb161
Pedro Belo <pedro@heroku.com>
2015-04-10 11:52:08 -0700

    explain the identifier

--- lib/template/config/initializers/database.rb
+++ lib/template/config/initializers/database.rb
@@ -1,4 +1,5 @@
 database_setup_proc = lambda do |conn|
+  # identify postgres connections coming from this process in pg_stat_activity
   process_identifier = ENV["DYNO"] || File.basename($0).gsub(/\W+/, "_")
   conn.execute "SET statement_timeout = '#{Config.database_timeout}s'"
   conn.execute "SET application_name = #{process_identifier}"
