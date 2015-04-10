5c1dac5422025e33dfa5d8ab66fa81709b13c5b7
Pedro Belo <pedro@heroku.com>
2015-04-10 11:03:55 -0700

    cosmetics

--- lib/template/config/initializers/database.rb
+++ lib/template/config/initializers/database.rb
@@ -1,6 +1,6 @@
 database_setup_proc = lambda do |conn|
-  conn.execute "set statement_timeout = '#{Config.database_timeout}s'"
-  conn.execute "set application_name = #{File.basename($0)}"
+  conn.execute "SET statement_timeout = '#{Config.database_timeout}s'"
+  conn.execute "SET application_name = #{File.basename($0)}"
 end
 
 Sequel.connect(Config.database_url,
