2a0c93ff702eff924284eaf9dbe776f114085c76
Pedro Belo <pedro@heroku.com>
2015-04-10 10:54:48 -0700

    cosmetics

--- lib/template/config/initializers/database.rb
+++ lib/template/config/initializers/database.rb
@@ -1,3 +1,7 @@
+database_setup_proc = lambda do |conn|
+  conn.execute "set statement_timeout = '#{Config.database_timeout}s'"
+end
+
 Sequel.connect(Config.database_url,
-               max_connections: Config.db_pool,
-               after_connect: -> (c) { c.execute "set statement_timeout = '#{Config.database_timeout}s'"  })
+  max_connections: Config.db_pool,
+  after_connect: database_setup_proc)
