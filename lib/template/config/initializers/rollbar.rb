3940d5b60a0a917ad80e8c7b1e67afc6c8924fae
Pavel Pravosud <pavel@pravosud.com>
2015-05-22 15:25:56 -0700

    Make Sinatra::NotFound error a warning

--- lib/template/config/initializers/rollbar.rb
+++ lib/template/config/initializers/rollbar.rb
@@ -2,5 +2,6 @@ Rollbar.configure do |config|
   config.enabled = ENV.has_key?('ROLLBAR_ACCESS_TOKEN')
   config.access_token = ENV["ROLLBAR_ACCESS_TOKEN"]
   config.use_sucker_punch
+  config.exception_level_filters.merge! "Sinatra::NotFound" => "warning"
 end
 
