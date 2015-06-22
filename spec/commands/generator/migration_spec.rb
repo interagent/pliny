29582aa20ed49fdc70f58638de4ca0848d30df6b
Dyego Costa <dyego@dyegocosta.com>
2015-06-22 16:15:09 +0100

    Simplify migration generator specs

--- spec/commands/generator/migration_spec.rb
+++ spec/commands/generator/migration_spec.rb
@@ -2,34 +2,18 @@ require 'pliny/commands/generator'
 require 'pliny/commands/generator/migration'
 require 'spec_helper'
 
-describe Pliny::Commands::Generator::Base do
-  subject { Pliny::Commands::Generator::Migration.new(migration_name, {}, StringIO.new) }
-
+describe Pliny::Commands::Generator::Migration do
   describe '#name' do
-    shared_examples 'a migration name' do
-      it 'generates a snakecase name' do
-        assert_equal 'add_column_foo_to_barz', subject.name
+    it 'generates a migration name given differents argument formats' do
+      [
+        %w(add column foo to barz),
+        'add column foo to barz',
+        'add_column_foo_to_barz',
+        'AddColumnFooToBarz'
+      ].each do |argument|
+        actual = described_class.new(argument, {}, StringIO.new).name
+        assert_equal 'add_column_foo_to_barz', actual
       end
-    end
-
-    context 'given array of args' do
-      let(:migration_name) { %w(add column foo to barz) }
-      it_behaves_like 'a migration name'
-    end
-
-    context 'given string separated by whitepsaces' do
-      let(:migration_name) { 'add column foo to barz' }
-      it_behaves_like 'a migration name'
-    end
-
-    context 'given a snakecase name' do
-      let(:migration_name) { 'add_column_foo_to_barz' }
-      it_behaves_like 'a migration name'
-    end
-
-    context 'given a camelcase name' do
-      let(:migration_name) { 'AddColumnFooToBarz' }
-      it_behaves_like 'a migration name'
     end
   end
 end
