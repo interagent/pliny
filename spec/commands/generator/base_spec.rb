7675f6082d87c115a63e184cf147280c5fe73ba0
Dyego Costa <dyego@dyegocosta.com>
2015-07-03 22:53:28 +0200

    Make specs compatible with Ruby 1.9.3

--- spec/commands/generator/base_spec.rb
+++ spec/commands/generator/base_spec.rb
@@ -3,7 +3,7 @@ require 'pliny/commands/generator/base'
 require 'spec_helper'
 
 describe Pliny::Commands::Generator::Base do
-  def generator(name:, options: {}, stream: StringIO.new)
+  def generator(name, options = {}, stream = StringIO.new)
     Pliny::Commands::Generator::Base.new(name, options, stream)
   end
 
@@ -16,7 +16,7 @@ describe Pliny::Commands::Generator::Base do
         'resource_history',
         'ResourceHistory'
       ].each do |argument|
-        actual = generator(name: argument).name
+        actual = generator(argument).name
         assert_equal 'resource_history', actual
       end
     end
@@ -24,34 +24,34 @@ describe Pliny::Commands::Generator::Base do
 
   describe '#singular_class_name' do
     it 'builds a class name for an endpoint' do
-      actual = generator(name: 'resource_histories').singular_class_name
+      actual = generator('resource_histories').singular_class_name
       assert_equal 'ResourceHistory', actual
     end
   end
 
   describe '#plural_class_name' do
     it 'builds a class name for a model' do
-      actual = generator(name: 'resource_histories').plural_class_name
+      actual = generator('resource_histories').plural_class_name
       assert_equal 'ResourceHistories', actual
     end
   end
 
   describe '#field_name' do
     it 'uses the singular form' do
-      actual = generator(name: 'resource_histories').field_name
+      actual = generator('resource_histories').field_name
       assert_equal 'resource_history', actual
     end
   end
 
   describe '#pluralized_file_name' do
     it 'uses the plural form' do
-      actual = generator(name: 'resource_history').pluralized_file_name
+      actual = generator('resource_history').pluralized_file_name
       assert_equal 'resource_histories', actual
     end
 
     describe 'when name with slashs' do
       it 'handles slashs as directory' do
-        actual = generator(name: 'resource/history').pluralized_file_name
+        actual = generator('resource/history').pluralized_file_name
         assert_equal 'resource/histories', actual
       end
     end
@@ -59,13 +59,13 @@ describe Pliny::Commands::Generator::Base do
 
   describe '#table_name' do
     it 'uses the plural form' do
-      actual = generator(name: 'resource_history').table_name
+      actual = generator('resource_history').table_name
       assert_equal 'resource_histories', actual
     end
 
     describe 'when name with slashs' do
       it 'handles slashs as underscores' do
-        actual = generator(name: 'resource/history').table_name
+        actual = generator('resource/history').table_name
         assert_equal 'resource_histories', actual
       end
     end
