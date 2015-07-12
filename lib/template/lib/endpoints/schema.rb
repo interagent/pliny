2fd5a1179a5fe428c53a8958c12cbd0e62d2304b
Guðmundur Bjarni Ólafsson <gudmundur.bjarni@gmail.com>
2015-07-12 16:19:29 +0200

    Improve schema.rb styling

--- lib/template/lib/endpoints/schema.rb
+++ lib/template/lib/endpoints/schema.rb
@@ -1,10 +1,11 @@
 module Endpoints
   class Schema < Base
     get "/schema.json" do
-      content_type("application/schema+json")
-      response.headers["Cache-Control"] = "public, max-age=3600"
+      content_type "application/schema+json"
+      headers["Cache-Control"] = "public, max-age=3600"
       unless File.exists?(schema_filename)
-        raise Pliny::Errors::NotFound.new("This application does not have a schema file.")
+        message = "This application does not have a schema file."
+        raise Pliny::Errors::NotFound.new(message)
       end
       File.read(schema_filename)
     end
