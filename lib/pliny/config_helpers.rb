8ea2718eee982b8c156e9cd807743a68d4403ad8
Yannick <yannick.schutz@gmail.com>
2015-07-14 10:39:27 +0200

    Use casting for array

--- lib/pliny/config_helpers.rb
+++ lib/pliny/config_helpers.rb
@@ -35,8 +35,18 @@ module Pliny
       ->(v) { v.to_sym }
     end
 
-    def array
-      ->(v) { v.split(",") }
+    # optional :accronyms, array(string)
+    # => ['a', 'b']
+    # optional :numbers, array(int)
+    # => [1, 2]
+    # optional :notype, array
+    # => ['a', 'b']
+    def array(method = nil)
+      -> (v) do
+        if v
+          v.split(",").map{|a| cast(a, method) }
+        end
+      end
     end
 
     private
