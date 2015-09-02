b381f7293051c97b4df5fa498711de7a329bc7fe
Pedro Belo <pedro@heroku.com>
2015-09-02 11:42:30 -0700

    move spec coverage support to spec_support

---
+++ lib/template/spec/spec_support/coverage.rb
@@ -0,0 +1,15 @@
+# setting ENV["CI"] configures simplecov for continuous integration output
+# setting ENV["COVERAGE"] generates a report when running tests locally
+if ENV["COVERAGE"] || ENV["CI"]
+  require "simplecov"
+  if ENV["CI"]
+    SimpleCov.formatter = SimpleCov::Formatter::SimpleFormatter
+    SimpleCov.at_exit do
+      puts SimpleCov.result.format!
+    end
+  end
+  SimpleCov.start do
+    # without this the simple formatter won't do anything
+    add_group "lib", "lib"
+  end
+end
