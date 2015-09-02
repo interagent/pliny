15d0ef92438349d12e30386c9cbc5f91a769ce42
Pedro Belo <pedro@heroku.com>
2015-09-02 11:36:56 -0700

    order overrided config

--- lib/template/config/config.rb
+++ lib/template/config/config.rb
@@ -18,19 +18,19 @@ module Config
   optional :versioning_app_name, string
 
   # Override -- value is returned or the set default.
+  override :database_timeout, 10,    int
   override :db_pool,          5,    int
   override :deployment,       'production', string
+  override :force_ssl,        true,  bool
+  override :pliny_env,        'development', string
   override :port,             5000, int
+  override :pretty_json,      false, bool
   override :puma_max_threads, 16,   int
   override :puma_min_threads, 1,    int
   override :puma_workers,     3,    int
   override :rack_env,         'development', string
-  override :pliny_env,        'development', string
   override :raise_errors,     false,         bool
   override :root,             File.expand_path("../../", __FILE__), string
   override :timeout,          10,    int
-  override :database_timeout, 10,    int
-  override :force_ssl,        true,  bool
   override :versioning,       false, bool
-  override :pretty_json,      false, bool
 end
