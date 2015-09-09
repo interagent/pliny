17785a22842fd36b483bed994b543fa5c7df2083
Pedro Belo <pedro@heroku.com>
2015-09-09 09:02:40 -0700

    add spec for the encode helper

---
+++ spec/helpers/encode_spec.rb
@@ -0,0 +1,41 @@
+require "spec_helper"
+
+describe Pliny::Helpers::Encode do
+  def app
+    Sinatra.new do
+      helpers Pliny::Helpers::Encode
+      post "/" do
+        encode(params)
+      end
+    end
+  end
+
+  before do
+    stub(Config).pretty_json { false }
+  end
+
+  it "sets the Content-Type" do
+    post "/"
+    assert_equal "application/json;charset=utf-8",
+      last_response.headers["Content-Type"]
+  end
+
+  it "encodes as json" do
+    payload = { "foo" => "bar" }
+    post "/", payload
+    assert_equal MultiJson.encode(payload), last_response.body
+  end
+
+  it "encodes in pretty mode when pretty=true" do
+    payload = { "foo" => "bar", "pretty" => "true" }
+    post "/", payload
+    assert_equal MultiJson.encode(payload, pretty: true), last_response.body
+  end
+
+  it "encodes in pretty mode when set by config" do
+    stub(Config).pretty_json { true }
+    payload = { "foo" => "bar" }
+    post "/", payload
+    assert_equal MultiJson.encode(payload, pretty: true), last_response.body
+  end
+end
