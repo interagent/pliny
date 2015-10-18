a7a2af71468181fcda347b3184dae0f959c69ef5
Guðmundur Bjarni Ólafsson <gudmundur.bjarni@gmail.com>
2015-10-18 19:06:50 +0200

    Add GET /health/db endpoint

--- lib/template/lib/endpoints/health.rb
+++ lib/template/lib/endpoints/health.rb
@@ -2,7 +2,27 @@ module Endpoints
   class Health < Base
      namespace '/health' do
       get do
-        encode {}
+        encode({})
+      end
+
+      get '/db' do
+        database?
+        database_available?
+        encode({})
+      end
+
+      private
+
+      def database?
+        raise Pliny::Errors::NotFound if DB.nil?
+      end
+
+      def database_available?
+        raise Pliny::Errors::ServiceUnavailable unless DB.test_connection
+      rescue Sequel::Error => e
+        message = e.message.strip
+        Pliny.log(db: true, health: true, at: 'exception', message: message)
+        raise Pliny::Errors::ServiceUnavailable
       end
     end
   end
--- lib/template/spec/endpoints/health_spec.rb
+++ lib/template/spec/endpoints/health_spec.rb
@@ -1,6 +1,6 @@
 require "spec_helper"
 
-describe Endpoints::Health do
+RSpec.describe Endpoints::Health do
   include Rack::Test::Methods
 
   def app
@@ -10,6 +10,42 @@ describe Endpoints::Health do
   describe 'GET /health' do
     it 'returns a 200' do
       get '/health'
+      assert_equal(200, last_response.status)
+      assert_equal('application/json;charset=utf-8', last_response.headers['Content-Type'])
+      assert_equal(2, last_response.headers['Content-Length'].to_i)
+      assert_equal({}, MultiJson.decode(last_response.body))
+    end
+  end
+
+  describe 'GET /health/db' do
+    it 'raises a 404 when no database is available' do
+      allow(DB).to receive(:nil?).and_return(true)
+
+      assert_raises Pliny::Errors::NotFound do
+        get '/health/db'
+      end
+    end
+
+    it 'raises a 503 on Sequel exceptions' do
+      allow(DB).to receive(:test_connection).and_raise(Sequel::Error)
+
+      assert_raises Pliny::Errors::ServiceUnavailable do
+        get '/health/db'
+      end
+    end
+
+    it 'raises a 503 when connection testing fails' do
+      allow(DB).to receive(:test_connection).and_return(false)
+
+      assert_raises Pliny::Errors::ServiceUnavailable do
+        get '/health/db'
+      end
+    end
+
+    it 'returns a 200' do
+      allow(DB).to receive(:test_connection).and_return(true)
+
+      get '/health/db'
       assert_equal(200, last_response.status)
       assert_equal('application/json;charset=utf-8', last_response.headers['Content-Type'])
       assert_equal(2, last_response.headers['Content-Length'].to_i)
