918bbea85241ec90b4e94b8d9b7a9a73a8a1efef
Guðmundur Bjarni Ólafsson <gudmundur.bjarni@gmail.com>
2015-11-27 09:25:45 +0100

    Use fail instead of raise

--- lib/template/lib/endpoints/health.rb
+++ lib/template/lib/endpoints/health.rb
@@ -14,11 +14,11 @@ module Endpoints
       private
 
       def database?
-        raise Pliny::Errors::NotFound if DB.nil?
+        fail Pliny::Errors::NotFound if DB.nil?
       end
 
       def database_available?
-        raise Pliny::Errors::ServiceUnavailable unless DB.test_connection
+        fail Pliny::Errors::ServiceUnavailable unless DB.test_connection
       rescue Sequel::Error => e
         message = e.message.strip
         Pliny.log(db: true, health: true, at: 'exception', message: message)
