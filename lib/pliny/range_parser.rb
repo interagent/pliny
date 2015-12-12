84649059786a09a0df19460bd33b3dcc713c7409
Guðmundur Bjarni Ólafsson <gudmundur.bjarni@gmail.com>
2015-12-12 15:50:17 +0100

    Add RangeParser with attributes

---
+++ lib/pliny/range_parser.rb
@@ -0,0 +1,25 @@
+module Pliny
+  class RangeParser
+    attr_reader :range_header
+    attr_reader :start, :end, :parameters
+
+    def initialize(range_header)
+      @range_header = range_header
+
+      set_defaults
+      parse
+    end
+
+    private
+
+    def parse
+
+    end
+
+    def set_defaults
+      @start = nil
+      @end = nil
+      @parameters = {}
+    end
+  end
+end
