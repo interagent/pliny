4a83438262f0d7fb44414a54880d85be6adee4b8
Pedro Belo <pedro@heroku.com>
2016-01-06 20:13:36 -0800

    configure pliny specs to use locales from the template

---
+++ spec/support/i18n.rb
@@ -0,0 +1,4 @@
+# configure i18n to use locales from the template app
+I18n.config.enforce_available_locales = true
+I18n.load_path += Dir["./lib/template/config/locales/*.{rb,yml}"]
+I18n.backend.load_translations
