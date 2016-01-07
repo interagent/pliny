57779eff42515ae6461351c958db14335315ab80
Pedro Belo <pedro@heroku.com>
2016-01-06 19:26:26 -0800

    add I18n to the template app

--- lib/template/lib/initializer.rb
+++ lib/template/lib/initializer.rb
@@ -1,6 +1,7 @@
 module Initializer
   def self.run
     require_config
+    load_locales
     require_lib
     require_initializers
     require_models
@@ -8,6 +9,12 @@ module Initializer
 
   def self.require_config
     require_relative "../config/config"
+  end
+
+  def self.load_locales
+    I18n.config.enforce_available_locales = true
+    I18n.load_path += Dir[Config.root + "/config/locales/*.{rb,yml}"]
+    I18n.backend.load_translations
   end
 
   def self.require_lib
