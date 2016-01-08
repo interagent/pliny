85186395c60bc5e5d97da082b95a5e30ed467d1b
Pedro Belo <pedro@heroku.com>
2016-01-07 16:54:45 -0800

    oops, make clear this is a method

--- lib/pliny/errors.rb
+++ lib/pliny/errors.rb
@@ -25,37 +25,37 @@ module Pliny
       end
     end
 
-    def self.MakeError(status, id)
+    def self.make_error(status, id)
       Class.new(Pliny::Errors::Error) do
         @error_class_id = id
         @error_class_status = status
       end
     end
 
-    BadRequest                   = MakeError(400, :bad_request)
-    Unauthorized                 = MakeError(401, :unauthorized)
-    PaymentRequired              = MakeError(402, :payment_required)
-    Forbidden                    = MakeError(403, :forbidden)
-    NotFound                     = MakeError(404, :not_found)
-    MethodNotAllowed             = MakeError(405, :method_not_allowed)
-    NotAcceptable                = MakeError(406, :not_acceptable)
-    ProxyAuthenticationRequired  = MakeError(407, :proxy_authentication_required)
-    RequestTimeout               = MakeError(408, :request_timeout)
-    Conflict                     = MakeError(409, :conflict)
-    Gone                         = MakeError(410, :gone)
-    LengthRequired               = MakeError(411, :length_required)
-    PreconditionFailed           = MakeError(412, :precondition_failed)
-    RequestEntityTooLarge        = MakeError(413, :request_entity_too_large)
-    RequestURITooLong            = MakeError(414, :request_uri_too_long)
-    UnsupportedMediaType         = MakeError(415, :unsupported_media_type)
-    RequestedRangeNotSatisfiable = MakeError(416, :requested_range_not_satisfiable)
-    ExpectationFailed            = MakeError(417, :expectation_failed)
-    UnprocessableEntity          = MakeError(422, :unprocessable_entity)
-    TooManyRequests              = MakeError(429, :too_many_requests)
-    InternalServerError          = MakeError(500, :internal_server_error)
-    NotImplemented               = MakeError(501, :not_implemented)
-    BadGateway                   = MakeError(502, :bad_gateway)
-    ServiceUnavailable           = MakeError(503, :service_unavailable)
-    GatewayTimeout               = MakeError(504, :gateway_timeout)
+    BadRequest                   = make_error(400, :bad_request)
+    Unauthorized                 = make_error(401, :unauthorized)
+    PaymentRequired              = make_error(402, :payment_required)
+    Forbidden                    = make_error(403, :forbidden)
+    NotFound                     = make_error(404, :not_found)
+    MethodNotAllowed             = make_error(405, :method_not_allowed)
+    NotAcceptable                = make_error(406, :not_acceptable)
+    ProxyAuthenticationRequired  = make_error(407, :proxy_authentication_required)
+    RequestTimeout               = make_error(408, :request_timeout)
+    Conflict                     = make_error(409, :conflict)
+    Gone                         = make_error(410, :gone)
+    LengthRequired               = make_error(411, :length_required)
+    PreconditionFailed           = make_error(412, :precondition_failed)
+    RequestEntityTooLarge        = make_error(413, :request_entity_too_large)
+    RequestURITooLong            = make_error(414, :request_uri_too_long)
+    UnsupportedMediaType         = make_error(415, :unsupported_media_type)
+    RequestedRangeNotSatisfiable = make_error(416, :requested_range_not_satisfiable)
+    ExpectationFailed            = make_error(417, :expectation_failed)
+    UnprocessableEntity          = make_error(422, :unprocessable_entity)
+    TooManyRequests              = make_error(429, :too_many_requests)
+    InternalServerError          = make_error(500, :internal_server_error)
+    NotImplemented               = make_error(501, :not_implemented)
+    BadGateway                   = make_error(502, :bad_gateway)
+    ServiceUnavailable           = make_error(503, :service_unavailable)
+    GatewayTimeout               = make_error(504, :gateway_timeout)
   end
 end
