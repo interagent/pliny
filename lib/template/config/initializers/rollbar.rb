526f764f511f4ebfed8d6609069f4d5ec485bb13
Guðmundur Bjarni Ólafsson <gudmundur.bjarni@gmail.com>
2016-03-18 15:17:15 +0100

    Use ROLLBAR_ENV rather than ROLLBAR_ENVIRONMENT

--- lib/template/config/initializers/rollbar.rb
+++ lib/template/config/initializers/rollbar.rb
@@ -1,7 +1,7 @@
 Rollbar.configure do |config|
   config.enabled = ENV.has_key?('ROLLBAR_ACCESS_TOKEN')
   config.access_token = ENV["ROLLBAR_ACCESS_TOKEN"]
-  config.environment = ENV['ROLLBAR_ENVIRONMENT']
+  config.environment = ENV['ROLLBAR_ENV']
   config.use_sucker_punch
 end
 
