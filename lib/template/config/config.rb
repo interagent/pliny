8865e7a75a09d16c226043e3035b136c5212cf82
Gudmundur Bjarni Olafsson <gudmundur.bjarni@gmail.com>
2016-05-12 18:18:22 +0200

    Alignment

--- lib/template/config/config.rb
+++ lib/template/config/config.rb
@@ -18,18 +18,18 @@ module Config
   optional :versioning_app_name, string
 
   # Override -- value is returned or the set default.
-  override :database_timeout, 10,    int
-  override :db_pool,          5,    int
+  override :database_timeout, 10,           int
+  override :db_pool,          5,            int
   override :deployment,       'production', string
-  override :force_ssl,        true,  bool
+  override :force_ssl,        true,         bool
   override :pliny_env,        'production', string
-  override :port,             5000, int
-  override :pretty_json,      false, bool
-  override :puma_max_threads, 16,   int
-  override :puma_min_threads, 1,    int
-  override :puma_workers,     3,    int
-  override :raise_errors,     false,         bool
+  override :port,             5000,         int
+  override :pretty_json,      false,        bool
+  override :puma_max_threads, 16,           int
+  override :puma_min_threads, 1,            int
+  override :puma_workers,     3,            int
+  override :raise_errors,     false,        bool
   override :root,             File.expand_path("../../", __FILE__), string
-  override :timeout,          10,    int
-  override :versioning,       false, bool
+  override :timeout,          10,           int
+  override :versioning,       false,        bool
 end
