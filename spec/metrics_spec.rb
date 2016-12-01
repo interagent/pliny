8156b8ab9a672fb85723517508761b4b1f74b621
Andrew Appleton <andysapple@gmail.com>
2016-12-01 15:40:46 +0000

    Abstract metrics interface from backend
    - Create a “Logger” backend for metrics
    - Use the logger backend by default in the metrics module
    - Allow different metrics backends to be set and unset

--- spec/metrics_spec.rb
+++ spec/metrics_spec.rb
@@ -1,61 +1,58 @@
 require "spec_helper"
 
 describe Pliny::Metrics do
-  let(:io) { double }
-
   subject(:metrics) { Pliny::Metrics }
 
-  before do
-    @stdout = Pliny.stdout
-    Pliny.stdout = io
+  let(:test_backend) { double(count: nil, measure: nil) }
 
-    allow(io).to receive(:print)
+  before do
     allow(Config).to receive(:app_name).and_return('pliny')
   end
 
   after do
-    Pliny.stdout = @stdout
+    Pliny::Metrics.reset_backend
   end
 
-  context "#count" do
-    it "counts a single key with a default value" do
+  it "defaults to the Logger backend" do
+    assert_equal Pliny::Metrics.backend, Pliny::Metrics::Backends::Logger
+  end
+
+  it "can set a different backend" do
+    Pliny::Metrics.backend = test_backend
+    assert_equal Pliny::Metrics.backend, test_backend
+  end
+
+  describe "#count" do
+    before { Pliny::Metrics.backend = test_backend }
+
+    it "delegates to the backend with a default value" do
       metrics.count(:foo)
-      expect(io).to have_received(:print).with("count#pliny.foo=1\n")
+      expect(test_backend).to have_received(:count).once.with(:foo, value: 1)
     end
 
-    it "counts a single key with a provided value" do
+    it "delegates to the backend with a provided value" do
       metrics.count(:foo, value: 2)
-      expect(io).to have_received(:print).with("count#pliny.foo=2\n")
+      expect(test_backend).to have_received(:count).once.with(:foo, value: 2)
     end
 
-    it "counts multiple keys" do
+    it "delegates multiple key counts to the backend" do
       metrics.count(:foo, :bar)
-      expect(io).to have_received(:print).with(
-        "count#pliny.foo=1 count#pliny.bar=1\n")
+      expect(test_backend).to have_received(:count).once.with(:foo, :bar, value: 1)
     end
   end
 
-  context "#measure" do
-    before do
-      Timecop.freeze(Time.now)
+  describe "#measure" do
+    before { Pliny::Metrics.backend = test_backend }
+    let(:block) { Proc.new {} }
+
+    it "delegates to the backend with a single key" do
+      metrics.measure(:foo, &block)
+      expect(test_backend).to have_received(:measure).once.with(:foo, &block)
     end
 
-    it "measures a single key" do
-      metrics.measure(:foo) { }
-      expect(io).to have_received(:print).with("measure#pliny.foo=0.000\n")
-    end
-
-    it "measures a single key over a minute" do
-      metrics.measure(:foo) do
-        Timecop.travel(60)
-      end
-      expect(io).to have_received(:print).with("measure#pliny.foo=60.000\n")
-    end
-
-    it "measures multiple keys" do
-      metrics.measure(:foo, :bar) { }
-      expect(io).to have_received(:print).with(
-        "measure#pliny.foo=0.000 measure#pliny.bar=0.000\n")
+    it "delegates to the backend with multiple keys" do
+      metrics.measure(:foo, :bar, &block)
+      expect(test_backend).to have_received(:measure).once.with(:foo, :bar, &block)
     end
   end
 end
