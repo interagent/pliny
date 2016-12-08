daba9fabe19fa0bfd63badc6d4dd47838ba5588a
Josh W Lewis <josh.w.lewis@gmail.com>
2016-12-08 12:52:51 -0600

    Add metrics logging middleware

---
+++ lib/pliny/middleware/metrics.rb
@@ -0,0 +1,28 @@
+module Pliny::Middleware
+  class Metrics
+    def initialize(app)
+      @app = app
+    end
+
+    def call(env)
+      start = Time.now
+
+      Pliny::Metrics.count("requests")
+
+      begin
+        status, headers, body = @app.call(env)
+      rescue
+        status = 500
+        raise
+      ensure
+        elapsed = (Time.now - start).to_f
+        Pliny::Metrics.measure("requests.latency", value: elapsed)
+
+        status_level = "#{status/100}xx"
+        Pliny::Metrics.count("requests.status.#{status_level}")
+      end
+
+      [status, headers, body]
+    end
+  end
+end
---
+++ spec/middleware/metrics_spec.rb
@@ -0,0 +1,61 @@
+require "spec_helper"
+
+describe Pliny::Middleware::Instruments do
+  def app
+    Rack::Builder.new do
+      run Sinatra.new {
+        use Pliny::Middleware::Metrics
+
+        get "/hello" do
+          Timecop.travel(5)
+          status 200
+          "hi"
+        end
+
+        get "/error" do
+          status 503
+          "boom"
+        end
+      }
+    end
+  end
+
+  before do
+    Timecop.freeze(Time.now)
+    allow(Pliny::Metrics).to receive(:count)
+    allow(Pliny::Metrics).to receive(:measure)
+  end
+
+  it "counts total requests" do
+    expect(Pliny::Metrics).to receive(:count).with("requests").once
+
+    get "/hello"
+  end
+
+  it "counts 2xx statuses" do
+    expect(Pliny::Metrics).to receive(:count).with("requests.status.2xx").once
+
+    get "/hello"
+  end
+
+  it "counts 4xx statuses" do
+    expect(Pliny::Metrics).to receive(:count).with("requests.status.4xx").once
+
+    get "/not-found"
+  end
+
+  it "counts 5xx statuses" do
+    expect(Pliny::Metrics).to receive(:count).with("requests.status.5xx").once
+
+    get "/error"
+  end
+
+  it "measures the request latency" do
+    expect(Pliny::Metrics).to receive(:measure) do |key, opts|
+      assert_equal(key, "requests.latency")
+      assert(4 < opts[:value] && opts[:value] < 6)
+    end
+
+    get "/hello"
+  end
+end
