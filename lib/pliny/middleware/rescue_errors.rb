48a6ac377940f555666b0277755f1bbe10c7c41f
Brandur <brandur@mutelight.org>
2016-12-12 12:58:43 -0800

    Store errors in `pliny.error` env key

--- lib/pliny/middleware/rescue_errors.rb
+++ lib/pliny/middleware/rescue_errors.rb
@@ -6,14 +6,20 @@ module Pliny::Middleware
     end
 
     def call(env)
+      error = nil
       @app.call(env)
     rescue Pliny::Errors::Error => e
+      error = e
       Pliny::Errors::Error.render(e)
     rescue => e
+      error = e
       raise if @raise
 
       Pliny::ErrorReporters.notify(e, rack_env: env)
       Pliny::Errors::Error.render(Pliny::Errors::InternalServerError.new)
+    ensure
+      # Leave this in env for CanonicalLogLineEmitter.
+      env["pliny.error"] = e
     end
   end
 end
