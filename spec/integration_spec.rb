db5c1e3a96a66ab96e0fd8f6e4342a658257e2ab
Tom Reznick <treznick+github@heroku.com>
2021-09-24 13:49:06 -0400

    fix the specs and provide better isolation

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -3,30 +3,44 @@ require "open3"
 require 'active_support/core_ext/object/blank'
 
 describe "Pliny integration test" do
-  before(:all) do
-    @original_dir = Dir.pwd
-
-    test_dir = Dir.mktmpdir("plinytest-")
-    Dir.chdir(test_dir)
-
-    bash "pliny-new myapp"
-
-    Dir.chdir("myapp")
-    bash "bin/setup"
-  end
-
-  after(:all) do
-    Dir.chdir(@original_dir)
-  end
-
   describe "bin/setup" do
+    around do |example|
+      original_dir = Dir.pwd
+      test_dir = Dir.mktmpdir("plinytest-")
+      Dir.chdir(test_dir)
+
+      bash "pliny-new myapp"
+
+      Dir.chdir("myapp")
+      example.run
+      Dir.chdir(original_dir)
+    end
+
     it "generates .env" do
+      bash "bin/setup"
+
       assert File.exist?("./.env")
     end
   end
 
   describe "pliny-generate scaffold" do
-    before(:all) do
+    around do |example|
+      DB.tables.each { |t| DB.drop_table(t) }
+
+      original_dir = Dir.pwd
+      test_dir = Dir.mktmpdir("plinytest-")
+      Dir.chdir(test_dir)
+
+      bash "pliny-new myapp"
+
+      Dir.chdir("myapp")
+      bash "bin/setup"
+
+      example.run
+      Dir.chdir(original_dir)
+    end
+
+    before do
       bash "pliny-generate scaffold artist"
     end
 
@@ -48,34 +62,44 @@ describe "Pliny integration test" do
   end
 
   describe "rake db:migrate:status" do
-    it "returns a migration in the DOWN state when not migrated" do
+    around do |example|
       DB.tables.each { |t| DB.drop_table(t) }
 
+      original_dir = Dir.pwd
+      test_dir = Dir.mktmpdir("plinytest-")
+      Dir.chdir(test_dir)
+
+      bash "pliny-new myapp"
+
+      Dir.chdir("myapp")
+      bash "bin/setup"
+
+      example.run
+      Dir.chdir(original_dir)
+    end
+
+    it "returns a migration in the DOWN state when not migrated" do
       bash "pliny-generate model artist"
       migration_file = Dir.glob('db/migrate/*').first
 
       stdout, stderr = bash_with_output("rake db:migrate:status")
 
-      statuses = Hash[stdout.to_s.split(/\+[-]+\+[-]+\+/)[2..].map { |s| s.tr("\n", "") }.select(&:present?).map { |s| s.split("|").map { |s| s.tr(" ", "") }.select(&:present?).reverse }]
+      statuses = Hash[stdout.to_s.split(/\+[-]+\+[-]+\+/)[2..-1].map { |s| s.tr("\n", "") }.select(&:present?).map { |s| s.split("|").map { |s| s.tr(" ", "") }.select(&:present?).reverse }]
       assert statuses[migration_file.split("/").last] == "DOWN"
     end
 
     it "returns a migration in the UP state when not migrated" do
-      DB.tables.each { |t| DB.drop_table(t) }
-
       bash "pliny-generate model artist"
       migration_file = Dir.glob('db/migrate/*').first
       bash "rake db:migrate"
 
       stdout, stderr = bash_with_output("rake db:migrate:status")
 
-      statuses = Hash[stdout.to_s.split(/\+[-]+\+[-]+\+/)[2..].map { |s| s.tr("\n", "") }.select(&:present?).map { |s| s.split("|").map { |s| s.tr(" ", "") }.select(&:present?).reverse }]
+      statuses = Hash[stdout.to_s.split(/\+[-]+\+[-]+\+/)[2..-1].map { |s| s.tr("\n", "") }.select(&:present?).map { |s| s.split("|").map { |s| s.tr(" ", "") }.select(&:present?).reverse }]
       assert statuses[migration_file.split("/").last] == "UP"
     end
 
     it "returns a migration in the FILE MISSING state when the file is missing" do
-      DB.tables.each { |t| DB.drop_table(t) }
-
       bash "pliny-generate model artist"
       migration_file = Dir.glob('db/migrate/*').first
       bash "rake db:migrate"
@@ -84,7 +108,7 @@ describe "Pliny integration test" do
 
       stdout, stderr = bash_with_output("rake db:migrate:status")
 
-      statuses = Hash[stdout.to_s.split(/\+[-]+\+[-]+\+/)[2..].map { |s| s.tr("\n", "") }.select(&:present?).map { |s| s.split("|").map { |s| s.gsub(/(^[ ]+|[ ]+$)/, "") }.select(&:present?).reverse }]
+      statuses = Hash[stdout.to_s.split(/\+[-]+\+[-]+\+/)[2..-1].map { |s| s.tr("\n", "") }.select(&:present?).map { |s| s.split("|").map { |s| s.gsub(/(^[ ]+|[ ]+$)/, "") }.select(&:present?).reverse }]
       assert statuses[migration_file.split("/").last] == "FILE MISSING"
     end
   end
