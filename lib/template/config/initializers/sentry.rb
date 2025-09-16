cc04fb2dd501bdcaf89af3557b641cc290c585b2
Daniel Bright <daniel.bright@salesforce.com>
2025-09-16 16:37:31 -0700

    added defaults for ENV var

--- lib/template/config/initializers/sentry.rb
+++ lib/template/config/initializers/sentry.rb
@@ -7,8 +7,8 @@ Pliny::ErrorReporters.error_reporters << Pliny::ErrorReporters::Sentry
 Sentry.init do |config|
   config.dsn = ENV["SENTRY_DSN"]
   config.environment = ENV["SENTRY_ENV"] || ENV["RACK_ENV"]
-  config.enabled_environments = %w[production staging]
-  config.traces_sample_rate = 0.1
+  config.enabled_environments = ENV["SENTRY_ENABLED_ENVIRONMENTS"]&.split(",") || %w[production staging]
+  config.traces_sample_rate = ENV["SENTRY_TRACES_SAMPLE_RATE"]&.to_f || 0.1
 end
 
 Pliny.use Sentry::Rack::CaptureExceptions
